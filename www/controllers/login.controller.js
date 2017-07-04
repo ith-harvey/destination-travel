01f739d76c6807015618fb435c0f70fda006ca6f
Ian Harvey <ith.harvey@gmail.com>
2017-07-03 17:40:13 -0700

    finnished breaking up all of the controllers in to a controller folder

--- www/controllers/login.controller.js
+++ www/controllers/login.controller.js
@@ -1,10 +1,6 @@
 angular.module('starter.controllers',['ngCordovaOauth'])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout, $cordovaOauth) {
-
-
-
-
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -32,9 +28,6 @@ angular.module('starter.controllers',['ngCordovaOauth'])
     $scope.modal.show();
   };
 
-
-
-
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
 
@@ -46,8 +39,6 @@ angular.module('starter.controllers',['ngCordovaOauth'])
       console.log("Error -> " + error);
     })
 
-    console.log('Ballz2', $scope.loginData);
-
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
     $timeout(function() {
@@ -55,17 +46,3 @@ angular.module('starter.controllers',['ngCordovaOauth'])
     }, 1000);
   };
 })
-
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,5 @@
 // Ionic Starter App
 
-console.log('server start')
-
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
@@ -46,7 +44,8 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       url: '/home',
       views: {
         'menuContent': {
-          templateUrl: 'templates/home.html'
+          templateUrl: 'templates/home.html',
+          controller: 'home'
         }
       }
     })
