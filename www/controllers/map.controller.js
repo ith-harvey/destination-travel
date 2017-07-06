6d3db64e79e3a4558d91c320f4649556623e5d37
Ian Harvey <ith.harvey@gmail.com>
2017-07-06 08:53:33 -0700

    brought in search bar working through the controller aspect now

--- www/controllers/map.controller.js
+++ www/controllers/map.controller.js
@@ -1,13 +1,13 @@
 
 
 angular.module('starter').controller('MapCtrl', function($scope, $state, $cordovaGeolocation) {
-  var options = {timeout: 10000, enableHighAccuracy: true};
+  let options = {timeout: 10000, enableHighAccuracy: true};
 
   $cordovaGeolocation.getCurrentPosition(options).then(function(position) {
 
-    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-    var mapOptions = {
+    let mapOptions = {
         center: latLng,
         zoom: 15,
         mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -15,7 +15,7 @@ angular.module('starter').controller('MapCtrl', function($scope, $state, $cordov
 
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-    var marker = new google.maps.Marker({
+    let marker = new google.maps.Marker({
         map: $scope.map,
         animation: google.maps.Animation.DROP,
         position: latLng
@@ -29,6 +29,67 @@ angular.module('starter').controller('MapCtrl', function($scope, $state, $cordov
     console.log($scope.angSearchBox);
     // var searchBox = new google.maps.places.SearchBox($scope.angSearchBox);
     //   $scope.map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+
+       // Create the search box and link it to the UI element.
+       console.log('yup',$scope.mapSearch);
+
+       let searchBox = new google.maps.places.SearchBox($scope.mapSearch);
+       $scope.map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+       // Bias the SearchBox results towards current map's viewport.
+      //  map.addListener('bounds_changed', function() {
+      //    searchBox.setBounds(map.getBounds());
+      //  });
+
+       let markers = [];
+       // Listen for the event fired when the user selects a prediction and retrieve
+       // more details for that place.
+       searchBox.addListener('places_changed', function() {
+         let places = searchBox.getPlaces();
+
+         if (places.length == 0) {
+           return;
+         }
+
+         // Clear out the old markers.
+         markers.forEach(function(marker) {
+           marker.setMap(null);
+         });
+         markers = [];
+
+         // For each place, get the icon, name and location.
+         let bounds = new google.maps.LatLngBounds();
+         places.forEach(function(place) {
+           if (!place.geometry) {
+             console.log("Returned place contains no geometry");
+             return;
+           }
+           let icon = {
+             url: place.icon,
+             size: new google.maps.Size(71, 71),
+             origin: new google.maps.Point(0, 0),
+             anchor: new google.maps.Point(17, 34),
+             scaledSize: new google.maps.Size(25, 25)
+           };
+
+           // Create a marker for each place.
+           markers.push(new google.maps.Marker({
+             map: $scope.map,
+             icon: icon,
+             title: place.name,
+             position: place.geometry.location
+           }));
+
+           if (place.geometry.viewport) {
+             // Only geocodes have viewport.
+             bounds.union(place.geometry.viewport);
+           } else {
+             bounds.extend(place.geometry.location);
+           }
+         });
+         $scope.map.fitBounds(bounds);
+       });
 
 
 
