267202fd75c8b02bfc00d6fcf78f7ef949602770
Ian Harvey <ith.harvey@gmail.com>
2017-07-13 11:11:21 -0700

    can now access markers on map

--- www/controllers/cities.controller.js
+++ www/controllers/cities.controller.js
@@ -9,42 +9,41 @@ angular.module('starter')
   let savedCityMarkers = []
   let bounds = new google.maps.LatLngBounds();
 
-
-  let mapOptions = {
-      center: ({lat: 39.82, lng: -95.712}),
-      zoom: 2,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      mapTypeControl: false,
-      streetViewControl: false
-    };
-
-  $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-
-
   citiesService.all().then(cities => {
-    console.log(' all cities! --> ', cities);
     $scope.cities = cities.data.trips
   })
 
   citiesService.allCitiesByTrip($state.params.id).then(cities => {
-    console.log('cities! --> ', cities);
-    $scope.cities = cities.data.cities
 
-    // place a marker for each city
+    //load the map
+    let mapOptions = {
+        center: ({lat: 39.82, lng: -95.712}),
+        zoom: 2,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: false,
+        streetViewControl: false
+      };
+
+    $scope.cityMap = new google.maps.Map(document.getElementById("cityMap"), mapOptions);
+
+    //set the cities for ng-list
+    $scope.cities = cities.data.cities
+  //
+  //   // place a marker for each city
     $scope.cities.forEach( (city, index) => {
       let latlng = ({lat: Number(city.city_lat),lng: Number(city.city_lng)})
 
       let marker = new google.maps.Marker({
         position: latlng,
-        map: $scope.map,
+        map: $scope.cityMap,
+        animation: google.maps.Animation.DROP,
         label: index.toString()
       });
 
       bounds.extend(latlng);
       savedCityMarkers.push(marker)
     })
-    $scope.map.fitBounds(bounds)
+    $scope.cityMap.fitBounds(bounds)
   })
 
 
--- www/controllers/map.controller.js
+++ www/controllers/map.controller.js
@@ -1,9 +1,4 @@
-
-
-angular.module('starter').controller('markerMapCtrl', function($scope, $rootScope, $state, $cordovaGeolocation, mapService, gMarkersService) {
-  console.log($state.params.id, 'state params!!');
-
-  let options = {timeout: 10000, enableHighAccuracy: true};
+angular.module('starter').controller('markerMapCtrl', function($scope, $rootScope, $state, mapService, gMarkersService) {
   let geocoder = new google.maps.Geocoder();
   $scope.details
   $scope.mapWatchService = mapService
@@ -12,57 +7,64 @@ angular.module('starter').controller('markerMapCtrl', function($scope, $rootScop
   $scope.markerMapClass = 'marker-map-full'
   const gMarkers = []
   const savedGMarkers = []
+  bounds = new google.maps.LatLngBounds();
 
 
   // Map gets initialized to to world view
   let mapOptions = {
-      center: ({lat: 39.82, lng: -95.712}),
-      zoom: 2,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      mapTypeControl: false
-    };
+    center: ({
+      lat: 39.82,
+      lng: -95.712
+    }),
+    zoom: 2,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    mapTypeControl: false
+  };
 
-  $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  $scope.markerMap = new google.maps.Map(document.getElementById("markerMap"), mapOptions);
 
 
-  // Once geolocation is retreived from device a new map is rendered
+  // Once markers are retreived from device a new map is rendered
   gMarkersService.allMarkersByCity($state.params.id).then(markers => {
     console.log('what we get back from markers --> ', markers);
+
+    // place a marker for each city
+    markers.data.markers.forEach((dbmarker, index) => {
+      const latlng = ({
+        lat: Number(dbmarker.marker_lat),
+        lng: Number(dbmarker.marker_lng)
+      })
+      let marker = new google.maps.Marker({
+        position: latlng,
+        animation: google.maps.Animation.DROP,
+        map: $scope.markerMap,
+      });
+      bounds.extend(latlng);
+      savedGMarkers.push(marker)
+    })
+    $scope.markerMap.fitBounds(bounds)
   })
 
 
-    // let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    //
-    // let mapOptions = {
-    //     center: latLng,
-    //     zoom: 12,
-    //     mapTypeId: google.maps.MapTypeId.ROADMAP,
-    //     mapTypeControl: false
-    //   };
-    //
-    // $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    //
-    // let marker = new google.maps.Marker({
-    //     map: $scope.map,
-    //     animation: google.maps.Animation.DROP,
-    //     position: latLng
-    // });
-
-
- // Watches for fireSearchWithItemDetails to fire, once it does it checks for the object. If there is an object it runs search
+  // Watches for fireSearchWithItemDetails to fire, once it does it checks for the object. If there is an object it runs search
   $scope.$watch('mapWatchService.fireSearchWithItemDetails()', newVal => {
     if (typeof newVal === 'object') {
-    $scope.searchMap(newVal.formatted_address)
+      $scope.searchMap(newVal.formatted_address)
     }
   })
 
 
-// retreives lat lng using googles geocode
-  $scope.searchMap = function (address) {
-    geocoder.geocode( { 'address': address}, function(results, status) {
+  // retreives lat lng using googles geocode
+  $scope.searchMap = function(address) {
+    geocoder.geocode({
+      'address': address
+    }, function(results, status) {
       if (status === 'OK') {
         console.log('results -->', results);
-        let latlng = {lat: results[0].geometry.location.lat(), lng: results[0].geometry.location.lng()}
+        let latlng = {
+          lat: results[0].geometry.location.lat(),
+          lng: results[0].geometry.location.lng()
+        }
         $scope.updateMap(latlng)
       } else {
         alert('Geocode was not successful for the following reason: ' + status);
@@ -71,26 +73,26 @@ angular.module('starter').controller('markerMapCtrl', function($scope, $rootScop
   }
 
   // updates the map once the lat lng is retreived from geocoder has been run or rather (onblur)
-  $scope.updateMap = function (latLng) {
-    console.log('latlng -->',latLng)
+  $scope.updateMap = function(latLng) {
+    console.log('latlng -->', latLng)
 
 
-    $scope.map.setCenter(latLng)
-    $scope.map.setZoom(15)
+    $scope.markerMap.setCenter(latLng)
+    $scope.markerMap.setZoom(15)
 
-    $scope.$apply(function () {
+    $scope.$apply(function() {
       $scope.details = mapService.getSearchItemDetails()
 
       let image = {
-          url: $scope.details.icon,
-          scaledSize: new google.maps.Size(20, 20),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(0, 32)
+        url: $scope.details.icon,
+        scaledSize: new google.maps.Size(20, 20),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(0, 32)
       }
 
       let marker = new google.maps.Marker({
         position: latLng,
-        map: $scope.map,
+        map: $scope.markerMap,
         icon: image
       });
       gMarkers.push(marker)
@@ -101,31 +103,31 @@ angular.module('starter').controller('markerMapCtrl', function($scope, $rootScop
 
 
   // hides and shows the destination Display
-  $scope.displayFire = function () {
+  $scope.displayFire = function() {
     $scope.destinationDisplay ? $scope.destinationDisplay = false : $scope.destinationDisplay = true
   }
 
-  $scope.saveDestination = function () {
+  $scope.saveDestination = function() {
     console.log('saving destination');
 
   }
 
-  $scope.changeMapClass = function () {
+  $scope.changeMapClass = function() {
     $scope.markerMapClass = 'mapWithDestination'
 
   }
 
-  $scope.cancelSearch = function () {
+  $scope.cancelSearch = function() {
     $scope.result.location = ''
 
   }
 
 
-    $scope.onTextClick = function ($event) {
-      $event.target.select();
-    };
+  $scope.onTextClick = function($event) {
+    $event.target.select();
+  };
 
-  $scope.saveLocation = function (destinationObj) {
+  $scope.saveLocation = function(destinationObj) {
     let lat = mapService.getSearchItemDetails().geometry.location.lat()
     let lng = mapService.getSearchItemDetails().geometry.location.lng()
 
@@ -135,15 +137,18 @@ angular.module('starter').controller('markerMapCtrl', function($scope, $rootScop
     console.log(mapService.getSearchItemDetails().geometry.location.lat());
     console.log('inside of save location');
     let image = {
-        url: 'img/Gold_star.png',
-        scaledSize: new google.maps.Size(60, 60),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(0, 32)
+      url: 'img/Gold_star.png',
+      scaledSize: new google.maps.Size(60, 60),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(0, 32)
     }
 
     let marker = new google.maps.Marker({
-      position: ({lat, lng}),
-      map: $scope.map,
+      position: ({
+        lat,
+        lng
+      }),
+      map: $scope.markerMap,
       icon: image
     });
 
