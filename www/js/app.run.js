2ed4709737561634a76be1bec61e97350076f177
Ian Harvey <ith.harvey@gmail.com>
2017-07-19 13:10:28 -0700

    added back buttons to app

--- www/js/app.run.js
+++ www/js/app.run.js
@@ -1,13 +1,21 @@
 (function() {
   angular.module('starter').run(run)
 
-  run.$inject = ['$rootScope', '$state', 'SessionsService']
+  run.$inject = ['$rootScope', '$state', 'SessionsService', '$ionicViewSwitcher','$ionicHistory' ]
 
-  function run ($rootScope, $state, SessionsService,) {
+  function run ($rootScope, $state, SessionsService, $ionicViewSwitcher, $ionicHistory) {
+
+    $rootScope.goBackState = function() {
+      $ionicViewSwitcher.nextDirection('back');
+      $ionicHistory.goBack();
+    }
+
     $rootScope.$on('$stateChangeStart', function (evt, toState, toParams, fromState, fromParams) {
       var user = SessionsService.user
 
       SessionsService.refresh()
+
+
 
       if (toState.requiresLogin && !user.id) {
         var notification = 'You cannot access that page without logging in.'
