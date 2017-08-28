515408095dc765594c5122de4d6383f84386d392
Ian Harvey <ith.harvey@gmail.com>
2017-08-28 15:21:53 -0500

    fixed port

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express')
 var app = express();
+app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + "/www"));
-app.listen(process.env.PORT || 5000);
+
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
